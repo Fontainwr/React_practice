f9aaccff20a51393f1294bbff6df5aa6d3e916f3
fontain richardson <fontainwr@gmail.com>
2023-08-09 15:12:40 +0900

    Working on react config

---
+++ frontend/src/AppState.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+////////////////////////////
+// INITIAL STATE
+////////////////////////////
+
+const initialState = {
+  url: "http://frreactrailsbackend-ea61f9590f43.herokuapp.com"
+};
+
+
+////////////////////////////
+// REDUCER
+////////////////////////////
+
+const reducer = (state, action)
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom"; // Fixed typo in the import
 import "./styles.css";
-import {App} from "./components/App"
-import { createRoot } from "react-dom/client";
+import App from "./components/App"; // Removed curly braces around App import
+import { createRoot } from "react-dom"; // Fixed the import statement
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.querySelector("#root")
+);
 
 const root = createRoot(document.querySelector("#root"));
-root.render(<App/>)
+root.render(<App />);
